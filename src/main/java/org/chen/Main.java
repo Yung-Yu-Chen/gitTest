23f7dd6e30621656a7415ff84ace343e3c1fcc96
Richard Chen <charlie2225896@gmail.com>
2023-11-16 23:34:35 +0800

    third commit

--- src/main/java/org/chen/Main.java
+++ src/main/java/org/chen/Main.java
@@ -4,10 +4,16 @@ import org.junit.Test;
 
 public class Main {
     public static void main(String[] args) {
+        FunctionOne();
     }
 
     @Test
     public void test(){
         System.out.println("I test it!");
+    }
+
+
+    public static void FunctionOne(){
+        System.out.println("Function One");
     }
 }
