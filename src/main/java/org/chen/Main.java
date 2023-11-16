3b2a2f0aeb01132c38e878e1a988fb151b725ed7
Richard Chen <charlie2225896@gmail.com>
2023-11-16 23:32:42 +0800

    second commit

--- src/main/java/org/chen/Main.java
+++ src/main/java/org/chen/Main.java
@@ -1,7 +1,13 @@
 package org.chen;
 
+import org.junit.Test;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+    }
+
+    @Test
+    public void test(){
+        System.out.println("I test it!");
     }
 }
